280d351996d74e0e9a1cbf52359b23790cbbb2e5
safchain <safchain@gmail.com>
2013-02-05 22:47:56 +0100

    fix licence

--- jni/dsp/compressor/comp-interface.cpp
+++ jni/dsp/compressor/comp-interface.cpp
@@ -1,7 +1,6 @@
-/* Lame.java
-   A port of LAME for Android
-
-   Copyright (c) 2010 Ethan Chen
+/* comp-interface.cpp
+  
+   Copyright (c) 2013 Sylvain Afchain
 
    This library is free software; you can redistribute it and/or
    modify it under the terms of the GNU Lesser General Public
